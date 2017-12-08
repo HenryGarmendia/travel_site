755c0748600e0406beb4bbf13dc1251123424773
HenryGarmendia <me@henrygarmendia.com>
2017-12-07 21:47:44 -0600

    npm install gulp-watch and created gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+
+// everything on gulp revolves on task
+gulp.task('default', function() {
+	console.log('OMG!!! checking if gulp task is working...');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
