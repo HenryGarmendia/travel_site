32e220b8b8892e66ccb84790761baf9d5a345357
HenryGarmendia <me@henrygarmendia.com>
2017-12-07 22:13:31 -0600

    added css folder, app.css and added html lang attribute also modified the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,34 @@
-var gulp = require('gulp');
+// gulp dependencies
+var gulp 				= require('gulp'),
+		gulp_watch 	= require('gulp-watch');
 
-// everything on gulp revolves on task
+// everything on gulp revolves on tasks
 gulp.task('default', function() {
-	console.log('OMG!!! checking if gulp task is working...');
+	console.log('OMG!!! checking if Gulp task is working...');
 });
+
+gulp.task('html', function() {
+	console.log('Changes have been done to the HTML file...');
+});
+
+gulp.task('css', function() {
+	console.log('Changes have been done to the CSS file...');
+});
+
+// automating the app files
+gulp.task('gulp_watch', function() {
+	// HTML automation
+	gulp_watch('./app/index.html', function() {
+		gulp.start('html');
+	});
+
+	// any CSS automation
+	gulp_watch('./app/assets/css/**/*.css', function() {
+		gulp.start('css');
+	});
+});
+
+
 
 
 
