9a33deae87e950119bb188dd8867a759ddf3db78
HenryGarmendia <me@henrygarmendia.com>
2017-12-08 20:26:07 -0600

    install postcss-mixins into project and required in css.js

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -4,14 +4,15 @@ var gulp 					= require('gulp'),
 		autoprefixer	= require('autoprefixer'),
 		cssvars				= require('postcss-simple-vars'),
 		nested 				= require('postcss-nested'),
-		cssImport 		= require('postcss-import');
+		cssImport 		= require('postcss-import'),
+		mixins 				= require('postcss-mixins');
 
 // task dedicated to CSS styles
 gulp.task('css', function() {
 	// gulp will create automatically a temp folder and css file
 	console.log('Changes have been done to the CSS file...');
 	return gulp.src('./app/assets/css/app.css ')
-				 .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+				 .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
 				 .on('error', function(error) { // error handling function
 				 		console.log(error.toString());
 					 	this.emit('end');
