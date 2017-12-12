6e882c01ba036e5492e4b671089e0206eb1ac843
HenryGarmendia <me@henrygarmendia.com>
2017-12-11 23:04:06 -0600

    finished styling the header section

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -5,14 +5,15 @@ var gulp 					= require('gulp'),
 		cssvars				= require('postcss-simple-vars'),
 		nested 				= require('postcss-nested'),
 		cssImport 		= require('postcss-import'),
-		mixins 				= require('postcss-mixins');
+		mixins 				= require('postcss-mixins'),
+		hexrgba 			= require('postcss-hexrgba');
 
 // task dedicated to CSS styles
 gulp.task('css', function() {
 	// gulp will create automatically a temp folder and css file
 	console.log('Changes have been done to the CSS file...');
 	return gulp.src('./app/assets/css/app.css ')
-				 .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+				 .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
 				 .on('error', function(error) { // error handling function
 				 		console.log(error.toString());
 					 	this.emit('end');
