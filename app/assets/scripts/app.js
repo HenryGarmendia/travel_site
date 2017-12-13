32ec65254983555a366bbbc13a62cc2778cfc54b
HenryGarmendia <me@henrygarmendia.com>
2017-12-13 15:48:16 -0600

    added basic functionality to the menu bar

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,18 +1,4 @@
-var $ = require('jquery');
-// var Person = require('./modules/Person'); // old way or node way
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-	payTaxes() {
-		console.log(this.name + " now owes $0 in taxes.");
-	}
-}
 
-var hendogg = new Person ('Hendogg Garmendia', 'Red');
-hendogg.greet();
-
-var jane = new Adult('Jane Smith', 'Pink');
-jane.greet();
-jane.payTaxes();
-
-$('h1').remove();
+var mobile_menu = new MobileMenu(); 
---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+
+var self = this;
+
+class MobileMenu {
+
+	constructor() {
+		this.site_header = $('.site-header');
+		this.menu_icon = $('.site-header__menu-icon');
+		this.menu_content = $('.site-header__menu-content');
+		this.events();
+	}
+
+	events() {
+		 this.menu_icon.click(this.toggle_menu.bind(this));
+	}
+
+	toggle_menu() {
+		// reveal the navigation links
+		this.menu_content.toggleClass('site-header__menu-content--is-visible');
+		this.site_header.toggleClass('site-header--is-expanded');
+	}
+
+}
+
+export default MobileMenu;
