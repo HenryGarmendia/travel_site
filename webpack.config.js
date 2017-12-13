e666de9b2acddf593e9d9340e61ba92b4d9b4ac6
HenryGarmendia <me@henrygarmendia.com>
2017-12-13 02:09:11 -0600

    install babel-core babel-loader babel-preset-es2015 to make life a little easier

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,14 @@ module.exports = {
 	entry: "./app/assets/scripts/app.js",
 	output: { 
 		path: path.resolve(__dirname,  "./app/temp/scripts"),
-		filename: "app.js" }
+		filename: "app.js" },
+
+module: { 
+	loaders: [ 
+		{ loader: 'babel-loader',
+			query: { 
+				presets: [ 'es2015' ] },
+
+			test:  /\.js$/,
+			exclude: /node_modules/ } ] }
 }
