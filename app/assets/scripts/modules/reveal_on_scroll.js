95bc5d0153c463ae65585d3f0929734fd9fc64b4
HenryGarmendia <me@henrygarmendia.com>
2017-12-14 03:51:56 -0600

    finished targetting the feature items with waypoints

--- app/assets/scripts/modules/reveal_on_scroll.js
+++ app/assets/scripts/modules/reveal_on_scroll.js
@@ -1,16 +1,31 @@
 import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class RevealOnScroll {
 
 	constructor() {
 		this.items_reveal = $('.feature-item');
 		this.hide_initially();
+		this.create_waypoints();
 
 		console.log(this.items_reveal);
 	}
 
 	hide_initially () {
 		this.items_reveal.addClass('reveal-item');
+	}
+
+	create_waypoints () {
+		this.items_reveal.each(function() {
+			var current_item = this;
+			 new Waypoint({
+			 	element: current_item,
+			 	handler: function() {
+			 		$(current_item).addClass('reveal-item--is-visible');
+			 	},
+			 	offset: '85%'
+			 });
+		});
 	}
 }
 
